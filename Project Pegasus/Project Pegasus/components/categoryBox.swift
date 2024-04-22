88a810f50179b255a44851b23869ee053e01c70c
Giovanni Ercolano <75883612+giovanni-ercolano@users.noreply.github.com>
2024-04-22 18:09:18 +0200

    aggiunta possibilità di modificare il nome di una categoria

--- Project Pegasus/Project Pegasus/components/categoryBox.swift
+++ Project Pegasus/Project Pegasus/components/categoryBox.swift
@@ -12,6 +12,8 @@ struct CategoryBox: View {
     @Environment(\.colorScheme) var colorScheme
     @Query var categories: [Category] = []
     @State private var selectedItem = 0
+    @State private var isEditing = false
+    @State private var editedCategoryName = ""
     
     var body: some View {
         ZStack{
@@ -34,37 +36,62 @@ struct CategoryBox: View {
                 }
                 
                 TabView(selection: $selectedItem) {
-                    ForEach(categories, id: \.id) { category in
-                        HStack(alignment: .center, content: {
+                    ForEach(categories.indices, id: \.self) { index in
+                        let category = categories[index]
+                        HStack(alignment: .center) {
                             Spacer()
-                            Text(category.name.uppercased())
-                                .font(
-                                    Font.custom("Montserrat", size: 24)
-                                        .weight(.bold)
-                                )
-                                .foregroundColor(Color(hex: colorScheme == .dark ? category.color : category.darkColor)).padding(.leading,10)
+                            if index == selectedItem && isEditing {
+                                TextField("Nuovo nome", text: $editedCategoryName, onCommit: {
+                                    // Salvare le modifiche al nome della categoria
+                                    if !editedCategoryName.isEmpty {
+                                        categories[index].name = editedCategoryName
+                                        // Aggiungi qui il codice per salvare le modifiche nel tuo sistema di gestione dati
+                                        isEditing.toggle()
+                                    }
+                                })
+                                .font(Font.custom("Montserrat", size: 24).weight(.bold))
+                                .foregroundColor(Color(hex: colorScheme == .dark ? category.color : category.darkColor))
+                                .padding(.leading,10)
+                                .padding(.trailing,15)
+                            } else {
+                                Text(category.name.uppercased())
+                                    .font(
+                                        Font.custom("Montserrat", size: 24)
+                                            .weight(.bold)
+                                    )
+                                    .foregroundColor(Color(hex: colorScheme == .dark ? category.color : category.darkColor)).padding(.leading,10)
+                            }
+                            
                             Spacer(minLength: 10)
+                            
                             Button(action: {
-                                //edit category.name
+                                // Attiva la modalità di modifica quando il pulsante viene premuto
+                                selectedItem = index
+                                isEditing.toggle()
+                                if !isEditing {
+                                    // Salva le modifiche al nome della categoria
+                                    if !editedCategoryName.isEmpty {
+                                        categories[index].name = editedCategoryName
+                                        // Aggiungi qui il codice per salvare le modifiche nel tuo sistema di gestione dati
+                                    }
+                                }
                             }){
                                 Image(colorScheme == .dark ? "lightWrite" : "darkWrite")
                                     .resizable()
                                     .frame(width: 18, height: 18)
                                     .padding(.trailing,15)
                             }
-                        })
+                        }
                         .padding(.horizontal,20)
                         .padding(.bottom,25)
-                        .tag(categories.firstIndex(of: category) ?? 0)
-                        //da inserire il + per creare una nuova categoria
+                        .tag(index)
                     }
                 }
                 .tabViewStyle(.page)
                 .accentColor(colorScheme == .dark ? .white : .black)
                 .frame(height:80)
-                .background(LinearGradient(colors: colorScheme == .dark ? [Color(hex: categories[selectedItem].color).opacity(0.09), Color(hex: categories[selectedItem].color).opacity(0.6)] : [Color(hex: categories[selectedItem].darkColor).opacity(0.09), Color(hex: categories[selectedItem].darkColor).opacity(0.6)], startPoint: .leading, endPoint: .trailing).cornerRadius(10)
-                            
-                )
+                .background(LinearGradient(colors: colorScheme == .dark ? [Color(hex: categories[selectedItem].color).opacity(0.09), Color(hex: categories[selectedItem].color).opacity(0.6)] : [Color(hex: categories[selectedItem].darkColor).opacity(0.09), Color(hex: categories[selectedItem].darkColor).opacity(0.6)], startPoint: .leading, endPoint: .trailing).cornerRadius(10))
+                
                 
             }
         }
